12a870dcad138153d1098bbd18105aec0fccfc16
blessie <blessie@armis.tech>
2021-01-17 00:03:52 +0100

    game of 15

--- Games/Task/src/games/gameOfFifteen/GameOfFifteen.kt
+++ Games/Task/src/games/gameOfFifteen/GameOfFifteen.kt
@@ -1,5 +1,6 @@
 package games.gameOfFifteen
 
+import board.Cell
 import board.Direction
 import board.GameBoard
 import board.createGameBoard
@@ -10,5 +11,41 @@ import games.game.Game
  * When you finish, you can play the game by executing 'PlayGameOfFifteen'.
  */
 fun newGameOfFifteen(initializer: GameOfFifteenInitializer = RandomGameInitializer()): Game =
-    TODO()
+    GameOfFifteen(initializer)
+
+class GameOfFifteen(private val initializer: GameOfFifteenInitializer) : Game {
+    private val board = createGameBoard<Int?>(4)
+
+    override fun initialize() {
+        val values = initializer.initialPermutation
+        val randomIdx =  Pair((1..4).random(),(1..4).random())
+        var leftOutIndex = 14
+        board[Cell(randomIdx.first, randomIdx.second)] = null
+        board.getAllCells().forEachIndexed{ idx, cell ->
+            if(cell.i==randomIdx.first && cell.j==randomIdx.second){
+                board[cell] = null
+                leftOutIndex = idx
+            }
+            else{
+                if(idx > values.size-1){
+                    board[cell] = values[leftOutIndex]
+                }
+                else{
+                board[cell] = values[idx]
+                }
+            }
+        }
+    }
+
+    override fun canMove() = board.any { it == null }
+
+    override fun hasWon() = board.getAllCells().hashCode()
+
+    override fun processMove(direction: Direction) {
+
+    }
+
+    override fun get(i: Int, j: Int): Int? = board.run { get(getCell(i, j)) }
+}
+
 
